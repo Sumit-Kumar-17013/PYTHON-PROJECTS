d27588db1169806f6478cbf9deb7a9e918ef6460
Sumit Kumar <sumit.edu.17013@gmail.com>
2026-08-21 23:30:25 +0530

    Add files via upload

---
+++ [01] Greater Number/part1_[01]_greater.py
@@ -0,0 +1,12 @@
+#Greater value  of two number!!!!!
+
+num1 = int(input("Enter The First Number = "))
+num2 = int(input("Enter The Seconf Number = "))
+
+if(num1 > num2 ):
+    print("The Greater Number Is = " , num1)
+elif(num1 < num2):
+    print("The Greater Number Is = " , num2)
+else:
+    print("Not Valid")
+
