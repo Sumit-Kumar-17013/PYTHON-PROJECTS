d4c90182953e0dbd23730d48e3610577b78c7a01
Sumit Kumar <sumit.edu.17013@gmail.com>
2026-08-22 16:46:53 +0530

    [02] postive , negativ or Zero

---
+++ [02] postive , negativ or Zero/part1_[02]_pos_nev_zero.py
@@ -0,0 +1,10 @@
+n = int(input("Enter The Number = "))
+
+if(n > 0):
+    print("It's a Positive Number")
+elif(n < 0):
+    print("It's a Negative Number")
+elif(n == 0):
+    print("It's Zero")
+else:
+    print("Not Valid!!!!")
