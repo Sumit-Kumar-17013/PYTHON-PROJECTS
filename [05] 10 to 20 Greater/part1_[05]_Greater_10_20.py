47e9b02a9356bac866e9c3143c236370463fa28b
Sumit Kumar <sumit.edu.17013@gmail.com>
2026-08-27 00:23:24 +0530

    upload 10 to 20 Greater

---
+++ [05] 10 to 20 Greater/part1_[05]_Greater_10_20.py
@@ -0,0 +1,6 @@
+num = int(input("Enter The Number = "))
+
+if(10 < num < 20):
+    print("The number lies between 10 and 20 (exclusive).")
+else:
+    print("The number lies Not between 10 and 20 (exclusive).")
